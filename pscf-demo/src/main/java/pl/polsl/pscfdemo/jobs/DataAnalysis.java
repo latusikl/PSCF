3c568c53281dad9df53794d9d54b2441d5c2fe44
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-25 13:35:31 +0200

    sending emergency message

--- pscf-demo/src/main/java/pl/polsl/pscfdemo/jobs/DataAnalysis.java
+++ pscf-demo/src/main/java/pl/polsl/pscfdemo/jobs/DataAnalysis.java
@@ -3,6 +3,7 @@ package pl.polsl.pscfdemo.jobs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import pl.polsl.pscfdemo.dto.InputBrokerDto;
 import pl.polsl.pscfdemo.dto.OutputBrokerDto;
 import pl.polsl.pscfdemo.mqtt.out.OutputSender;
 import pl.polsl.pscfdemo.services.DataMemoryService;
@@ -11,21 +12,42 @@ import pl.polsl.pscfdemo.services.DataMemoryService;
 @Slf4j
 public class DataAnalysis {
 
-	final
-	DataMemoryService dataMemoryService;
-	final OutputSender outputSender;
+    private final DataMemoryService dataMemoryService;
+    private final OutputSender outputSender;
 
-	public DataAnalysis(final DataMemoryService dataMemoryService, final OutputSender outputSender) {
-		this.dataMemoryService = dataMemoryService;
-		this.outputSender = outputSender;
-	}
+    public DataAnalysis(final DataMemoryService dataMemoryService, final OutputSender outputSender) {
+        this.dataMemoryService = dataMemoryService;
+        this.outputSender = outputSender;
+    }
 
-	//TODO implement data processing according to project spec.
-	@Scheduled(fixedRate = 60000)
-	public void processData(){
-		OutputBrokerDto data = OutputBrokerDto.builder().accident(false).dose(10.0).emergencyStop(false).pumpOneState(true).pumpTwoState(false).build();
-		log.info(dataMemoryService.getAllMeasurements().get(0).getTimestamp().toString());
-		outputSender.sendToMqtt(data);
+    @Scheduled(fixedRate = 60000)
+    public void processData() {
+        if (dataMemoryService.getAllMeasurements().size() > 0) {
+            log.info(dataMemoryService.getAllMeasurements().get(0).getTimestamp().toString());
 
-	}
+            OutputBrokerDto data = OutputBrokerDto.builder()
+                    .accident(false)
+                    .dose(10.0).emergencyStop(false)
+                    .pumpOneState(true)
+                    .pumpTwoState(false)
+                    .build();
+            outputSender.sendToMqtt(data);
+        }
+    }
+
+    public void checkForAccident(final InputBrokerDto data) {
+        if (data.getAccident() || !data.getCarbonFilter() || !data.getGravelFilter()) {
+            OutputBrokerDto emergencyData = OutputBrokerDto.builder().accident(true).emergencyStop(true).build();
+            outputSender.sendToMqtt(emergencyData);
+            if (!data.getAccident()) {
+                log.info("WARNING!!! ACCIDENT! Date: " + data.getTimestamp());
+            }
+            if (!data.getCarbonFilter()) {
+                log.info("WARNING!!! Carbon filter needs to be replaced! Date: " + data.getTimestamp());
+            }
+            if (!data.getGravelFilter()) {
+                log.info("WARNING!!! Gravel filter needs to be replaced! Date: " + data.getTimestamp());
+            }
+        }
+    }
 }
--- pscf-demo/src/main/java/pl/polsl/pscfdemo/mqtt/in/InputMessageHandler.java
+++ pscf-demo/src/main/java/pl/polsl/pscfdemo/mqtt/in/InputMessageHandler.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Component;
 import pl.polsl.pscfdemo.dto.InputBrokerDto;
+import pl.polsl.pscfdemo.jobs.DataAnalysis;
 import pl.polsl.pscfdemo.services.DataMemoryService;
 
 /**
@@ -18,28 +19,31 @@ import pl.polsl.pscfdemo.services.DataMemoryService;
 @Slf4j
 public class InputMessageHandler implements MessageHandler {
 
-	final
-	ObjectMapper objectMapper;
-	final DataMemoryService dataMemoryService;
+    private final ObjectMapper objectMapper;
+    private final DataMemoryService dataMemoryService;
+    private final DataAnalysis dataAnalysis;
 
-	public InputMessageHandler(final ObjectMapper objectMapper, final DataMemoryService dataMemoryService) {
-		this.objectMapper = objectMapper;
-		this.dataMemoryService = dataMemoryService;
-	}
+    public InputMessageHandler(final ObjectMapper objectMapper, final DataMemoryService dataMemoryService, final DataAnalysis dataAnalysis) {
+        this.objectMapper = objectMapper;
+        this.dataMemoryService = dataMemoryService;
+        this.dataAnalysis = dataAnalysis;
+    }
 
-	/*
-	In this method you are receiving message.
-	 */
-	@Override
-	public void handleMessage(final Message<?> message) throws MessagingException {
-		log.info("New message received!");
-		final String messageBody = (String) message.getPayload();
-		try {
-			dataMemoryService.addMeasurement(objectMapper.readValue(messageBody, InputBrokerDto.class));
-			log.info("Object JSON mapped to Input DTO.");
-		} catch (JsonProcessingException e) {
-			log.error(e.getMessage());
-			throw new MessagingException(e.getMessage());
-		}
-	}
+    /*
+    In this method you are receiving message.
+     */
+    @Override
+    public void handleMessage(final Message<?> message) throws MessagingException {
+        log.info("New message received!");
+        final String messageBody = (String) message.getPayload();
+        try {
+            InputBrokerDto data = objectMapper.readValue(messageBody, InputBrokerDto.class);
+            dataAnalysis.checkForAccident(data);
+            dataMemoryService.addMeasurement(data);
+            log.info("Object JSON mapped to Input DTO.");
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+            throw new MessagingException(e.getMessage());
+        }
+    }
 }
